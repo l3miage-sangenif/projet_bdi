43c37b698f1815284450b34d333791f87f6b0f14
l3miage-sangenif <122084129+l3miage-sangenif@users.noreply.github.com>
2024-01-23 15:35:14 +0100

    script sql (#28)
    * script sql
    
    * script achat
    
    ---------
    
    Co-authored-by: 0atural <mouarafhouda@gmail.com>

---
+++ Back/src/main/resources/sql/region.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO Region (nom_region) VALUES ('Occitanie');
+INSERT INTO Region (nom_region) VALUES ('Provence-Alpes-Côte d Azur');
+INSERT INTO Region (nom_region) VALUES ('Nouvelle-Aquitaine');
+INSERT INTO Region (nom_region) VALUES ('Auvergne-Rhône-Alpes');
+INSERT INTO Region (nom_region) VALUES ('Bretagne');
+INSERT INTO Region (nom_region) VALUES ('Pays de la Loire');
+INSERT INTO Region (nom_region) VALUES ('Bourgogne-Franche-Comté');
+INSERT INTO Region (nom_region) VALUES ('Île-de-France');
+INSERT INTO Region (nom_region) VALUES ('Hauts-de-France');
+INSERT INTO Region (nom_region) VALUES ('Normandie');
+INSERT INTO Region (nom_region) VALUES ('Centre-Val de Loire');
+INSERT INTO Region (nom_region) VALUES ('Grand Est');
+INSERT INTO Region (nom_region) VALUES ('Corse');
+INSERT INTO Region (nom_region) VALUES ('Mayotte');
+INSERT INTO Region (nom_region) VALUES ('Guyane');
+INSERT INTO Region (nom_region) VALUES ('La Réunion');
+INSERT INTO Region (nom_region) VALUES ('Martinique');
+INSERT INTO Region (nom_region) VALUES ('Guadeloupe');
+INSERT INTO Region (nom_region) VALUES ('Nouvelle Calédonie');
+INSERT INTO Region (nom_region) VALUES ('Polynésie française');
+
