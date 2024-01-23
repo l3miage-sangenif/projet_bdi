2ab7ddeca6262ec0ddb424ea63130e5d3ffdd69c
l3miage-sangenif <122084129+l3miage-sangenif@users.noreply.github.com>
2024-01-23 16:11:21 +0100

    Feat get festival end point (#30)
    https://github.com/l3miage-sangenif/projet_bdi/pull/30

--- Back/src/main/java/fr/uga/miage/m1/controller/FestivalController.java
+++ Back/src/main/java/fr/uga/miage/m1/controller/FestivalController.java
@@ -1,13 +1,30 @@
 package fr.uga.miage.m1.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.uga.miage.m1.models.FestivalEntity;
+import fr.uga.miage.m1.service.FestivalService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/")
 public class FestivalController {
+
+    private final FestivalService festivalService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/festival")
+    public List<FestivalEntity> getAllFestival() {
+        return festivalService.getAllFestival();
+    }
     
 }
