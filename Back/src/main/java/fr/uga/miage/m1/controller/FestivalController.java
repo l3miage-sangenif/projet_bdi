54ee693ffa04cf85c02b617968de25c5f1cd6fdc
l3miage-sangenif <floreal.sangenis@etu.univ-grenoble-alpes.fr>
2024-01-24 09:55:24 +0100

    feat(back): create get festival by id end point

--- Back/src/main/java/fr/uga/miage/m1/controller/FestivalController.java
+++ Back/src/main/java/fr/uga/miage/m1/controller/FestivalController.java
@@ -7,10 +7,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.uga.miage.m1.DTO.Festival;
 import fr.uga.miage.m1.models.FestivalEntity;
 import fr.uga.miage.m1.service.FestivalService;
 
@@ -25,6 +27,13 @@ public class FestivalController {
     @GetMapping("/festival")
     public List<FestivalEntity> getAllFestival() {
         return festivalService.getAllFestival();
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/festival/{id}")
+    public FestivalEntity getFestivalById(@PathVariable Long id) {
+        return festivalService.getFestivalById(id);
     }
     
 }
